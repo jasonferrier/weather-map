be428a5c8e5a883110f24f7008be8bdaf8e7e8a6
jasonferrier <jason.ferrier@gmail.com>
2023-02-01 17:48:38 -0700

    Initial map loaded with Mapbox API key and centered to confluence of Green & CO Rivers

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useCallback, useState, useRef } from "react";
+import { Map, MapRef, ViewState } from "react-map-gl";
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const mapRef = useRef<MapRef>();
+  // const [viewState, setViewState]: ViewState = useState({
+  const [viewState, setViewState]: any = useState({
+    longitude: -109.885769,
+    latitude: 38.189371,
+    zoom: 10,
+  });
+
+  const onMapLoad = useCallback(() => {
+    mapRef?.current?.on("moveend", () => {
+      console.log("map moved");
+    });
+
+    mapRef?.current?.on("mouseup", (stuff) => {
+      console.log(`clicked at ${stuff.lngLat.lat}, ${stuff.lngLat.lng}`);
+      // console.log(stuff);
+    });
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Map
+        id="map-wrapper"
+        ref={mapRef}
+        {...viewState}
+        onMove={(evt) => setViewState(evt.viewState)}
+        onLoad={onMapLoad}
+        mapStyle="mapbox://styles/mapbox/outdoors-v11"
+        mapboxAccessToken="pk.eyJ1IjoiamFzb25mZXJyaWVyIiwiYSI6ImNsZGpjNXhyOTFkbDkzdW8zdjdsZDk4bXAifQ.16O4azQttMMckQyuutOI1w"
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
