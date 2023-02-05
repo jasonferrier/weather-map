40e8e1ad6a38fb452e7cdc8ba5c1e95b10f02bcd
jasonferrier <jason.ferrier@gmail.com>
2023-02-04 17:08:09 -0700

    Added Weather API query and displaying curent temp in popup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, useRef, ChangeEvent } from "react";
+import { useCallback, useState, useRef, ChangeEvent, useEffect } from "react";
 import Map, {
   FullscreenControl,
   LngLat,
@@ -12,17 +12,129 @@ import mapboxgl from "mapbox-gl";
 import "./App.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 
+const mapboxAccessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+// OpenWeather API Endpoint:
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+const openWeatherAPIKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
 type Marker = {
   position: Partial<LngLat> | null;
   show: boolean;
 };
 
-function App() {
-  const mapboxAccessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
-  // OpenWeather API Endpoint:
-  // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-  const openWeatherAPIKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+// TODO: ¿¿ Should I use this package: https://www.npmjs.com/package/openweathermap-ts ??
+// NOTE FROM OPENWEATHER API DOCUMENTATION:
+//    If you do not see some of the parameters in your API response it means that these
+//    weather phenomena are just not happened for the time of measurement for the city
+//    or location chosen. Only really measured or calculated data is displayed in API response.
+type weatherDataType = {
+  coord: {
+    lon: number; // City geo location, longitude
+    lat: number; // City geo location, latitude
+  };
+  weather: {
+    id: number; // Weather condition id
+    main: string; // Group of weather parameters (Rain, Snow, Extreme etc.)
+    description: string; // Weather condition within the group. You can get the output in your language. [Learn more](https://openweathermap.org/current#multi)
+    icon: string;
+  }[];
+  base: string; // **Internal parameter**
+  main: {
+    temp: number; // Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    feels_like: number; // Temperature. This temperature parameter accounts for the human perception of weather. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    temp_min: number; // Minimum temperature at the moment. This is minimal currently observed temperature (within large megalopolises and urban areas). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    temp_max: number; // Maximum temperature at the moment. This is maximal currently observed temperature (within large megalopolises and urban areas). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+    pressure: number; // Atmospheric pressure (on the sea level, if there is no sea_level or grnd_level data), hPa
+    humidity: number; // Humidity, %
+    sea_level: number; // Atmospheric pressure on the sea level, hPa
+    grnd_level: number; // Atmospheric pressure on the ground level, hPa
+  };
+  visibility: number; // Visibility, meter. The maximum value of the visibility is 10km
+  wind: {
+    speed: number; // Wind speed. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour.
+    deg: number; // Wind direction, degrees (meteorological)
+    gust: number; // Wind gust. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour
+  };
+  clouds: {
+    all: number; // Cloudiness, %
+  };
+  rain: {
+    "1h": number; // Rain volume for the last 1 hour, mm
+    "3h": number; // Rain volume for the last 3 hours, mm
+  };
+  snow: {
+    "1h": number; // Snow volume for the last 1 hour, mm
+    "3h": number; // Snow volume for the last 3 hours, mm
+  };
+  dt: number; // Time of data calculation, unix, UTC
+  sys: {
+    type: number; // **Internal parameter**
+    id: number; // **Internal parameter**
+    message: string; // **Internal parameter**
+    country: string; // Country code (GB, JP etc.)
+    sunrise: number; // Sunrise time, unix, UTC
+    sunset: number; // Sunset time, unix, UTC
+  };
+  timezone: number; // Shift in seconds from UTC
+  id: number; // City ID. Please note that built-in geocoder functionality has been deprecated.
+  name: string; // City name. Please note that built-in geocoder functionality has been deprecated. Learn more
+  cod: number; // **Internal parameter**
+};
 
+type WeatherDisplayProps = {
+  latitude: string;
+  longitude: string;
+};
+
+const WeatherDisplay: React.FC<WeatherDisplayProps> = (props) => {
+  const { latitude, longitude } = props;
+  const [weatherData, setWeatherData] =
+    useState<Partial<weatherDataType> | null>(null);
+
+  // TODO: This is firing off TWO API calls.
+  useEffect(() => {
+    getWeatherAtLocation(longitude, latitude);
+    console.log(`querying OpenWeather API @ ${latitude}, ${longitude}`);
+    [];
+  }, []);
+
+  const getWeatherAtLocation = (lng: string, lat: string) => {
+    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${openWeatherAPIKey}&units=imperial`;
+    fetch(weatherApiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response;
+      })
+      .then((response) => response.json())
+      .then((weather) => {
+        console.log(weather);
+        setWeatherData(weather);
+      });
+  };
+
+  return (
+    <div className="weather-wrapper">
+      {!weatherData && <p>Loading weather data…</p>}
+      {weatherData && (
+        <>
+          <p>Weather for this location is currently</p>
+          <table>
+            <tbody>
+              <tr>
+                <td>Temp (˚F)</td>
+                <td>{weatherData?.main?.temp || "no data"}</td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      )}
+    </div>
+  );
+};
+
+function App() {
   const mapRef = useRef<MapRef | null>(null);
 
   const [viewState, setViewState] = useState<ViewState>({
@@ -41,6 +153,9 @@ function App() {
   });
 
   const [showPopup, setShowPopup] = useState<boolean>(false);
+  const [popupHtml, setPopupHtml] = useState<string>(
+    "<p style='color: \"#333333\"'>Loading weather…</p>"
+  );
 
   const [marker, setMarker] = useState<Marker>({
     position: null,
@@ -194,13 +309,11 @@ function App() {
                 }}
                 // onOpen={() => console.log("popup open")}
               >
-                <p style={{ color: "#333333" }}>
-                  Coordinates:{" "}
-                  {coordinatesToLatLng(
-                    marker.position.lat,
-                    marker.position.lng
-                  )}
-                </p>
+                {/* TODO: Solve the re-rendering issue firing off another API call when map is panned and popup is reshown. */}
+                <WeatherDisplay
+                  latitude={marker.position.lat.toFixed(4)}
+                  longitude={marker.position.lng.toFixed(4)}
+                />
               </Popup>
             )}
           </>
